8c101a32984fe35e108fa57277561042ae1bbaeb
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-06-07 20:18:37 +0200

    upgraded display for form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import Container from "./Container";
 import Footer from "./Footer";
 import Form from "./Form";
 
+const currencies = [{
+	currency: "EUR",
+	worth: "4,48",
+	key: 1,
+},
+{
+	currency: "USD",
+	worth: "4,19",
+	key: 2,
+},
+{
+	currency: "GBP",
+	worth: "5,20",
+	key: 3,
+},
+]
 
 function App() {
 	return (
 		<Container>
-			<Form legend={"Kaltulator walut"} />
+			<Form currencies={currencies} legend={"Kaltulator walut"} />
 			<Footer body={"Wartość walut z dnia 03.02.2022 "} />
     </Container>
 	);
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,16 +1,18 @@
 import "./style.css";
 
-const Form = ({legend}) => (
-	<form className="js-form form">
+const Form = ({ legend, currencies}) => (
+	<form className="form">
 		<fieldset className="form__fieldset">
 			<legend className="form__legend">{legend}</legend>
 			<label className="form__label">
 				{" "}
 				Wybierz walutę
-				<select className="js-currency">
-					<option value="EUR">Euro</option>
-					<option value="USD">USD</option>
-					<option value="GBP">Funt Brytyjski</option>
+				<select className="form__select">
+					{currencies.map(currency => (
+						<option key={currency.key} value={currency.currency}>
+							{currency.currency}
+						</option>
+					))}
 				</select>
 			</label>
 			<br />
@@ -19,8 +21,11 @@ const Form = ({legend}) => (
 				Podaj kwotę w PLN:
 				<input type="number" min="0" step="any" className="js-pln" />
 			</label>
+			<button className="form__button" type="submit">
+				Wyślij
+			</button>
 			<p className="form__text">
-				Kwota po przeliczeniu wynosi: <strong className="js-result"> N/A</strong>
+				Kwota po przeliczeniu wynosi: <strong> N/A</strong>
 			</p>
 		</fieldset>
 	</form>
