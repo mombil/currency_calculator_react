b4362eaa44cdc47cae28a47632179b41a787bcaf
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-06-09 21:39:05 +0200

    added interactions to site

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@ import Container from "./Container";
 import Footer from "./Footer";
 import Form from "./Form";
 
-const currencies = [{
-	currency: "EUR",
-	worth: "4,48",
-	key: 1,
-},
-{
-	currency: "USD",
-	worth: "4,19",
-	key: 2,
-},
-{
-	currency: "GBP",
-	worth: "5,20",
-	key: 3,
-},
-]
+const currencies = [
+	{
+		currency: "EUR",
+		rate: 4.48,
+		key: 1,
+	},
+	{
+		currency: "USD",
+		rate: 4.19,
+		key: 2,
+	},
+	{
+		currency: "GBP",
+		rate: 5.2,
+		key: 3,
+	},
+];
 
 function App() {
 	return (
 		<Container>
 			<Form currencies={currencies} legend={"Kaltulator walut"} />
 			<Footer body={"Wartość walut z dnia 03.02.2022 "} />
-    </Container>
+		</Container>
 	);
 }
 
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,34 +1,84 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = ({ legend, currencies}) => (
-	<form className="form">
-		<fieldset className="form__fieldset">
-			<legend className="form__legend">{legend}</legend>
-			<label className="form__label">
-				{" "}
-				Wybierz walutę
-				<select className="form__select">
-					{currencies.map(currency => (
-						<option key={currency.key} value={currency.currency}>
-							{currency.currency}
-						</option>
-					))}
-				</select>
-			</label>
-			<br />
-			<br />
-			<label className="form__label">
-				Podaj kwotę w PLN:
-				<input type="number" min="0" step="any" className="js-pln" />
-			</label>
-			<button className="form__button" type="submit">
-				Wyślij
-			</button>
-			<p className="form__text">
-				Kwota po przeliczeniu wynosi: <strong> N/A</strong>
-			</p>
-		</fieldset>
-	</form>
-);
+const Form = ({ legend, currencies }) => {
+	const [amount, setAmount] = useState(0);
+	const [selects, setSelects] = useState("EUR");
+	const [result, setResult] = useState("N/A");
+	const [sign, setSign] = useState("");
+
+	const calculateResult = () => {
+		switch (selects) {
+			case "EUR":
+				setResult(amount / currencies[0].rate);
+				setSign("€");
+				break;
+
+			case "GBP":
+				setResult(amount / currencies[1].rate);
+				setSign("£");
+				break;
+
+			case "USD":
+				setResult(amount / currencies[2].rate);
+				setSign("$");
+				break;
+
+			default:
+		}
+	};
+
+	const onFormSubmit = event => {
+		event.preventDefault();
+		calculateResult();
+		setAmount(amount);
+	};
+
+	return (
+		<form className="form" onSubmit={onFormSubmit}>
+			<fieldset className="form__fieldset">
+				<legend className="form__legend">{legend}</legend>
+				<label className="form__label">
+					{" "}
+					Wybierz walutę
+					<select
+						className="form__select"
+						value={selects}
+						onChange={e => setSelects(e.target.value)}
+					>
+						{currencies.map(currency => (
+							<option key={currency.key} value={currency.currency}>
+								{currency.currency}
+							</option>
+						))}
+					</select>
+				</label>
+				<br />
+				<br />
+				<label className="form__label">
+					Podaj kwotę w PLN:
+					<input
+						type="number"
+						min="0"
+						step="any"
+						value={amount}
+						onChange={({ target }) => setAmount(target.value)}
+					/>
+				</label>
+				<button className="form__button" type="submit">
+					Wyślij
+				</button>
+				<p className="form__text">
+					Kwota po przeliczeniu wynosi:
+					<strong>
+						{typeof result === "string"
+							? " N/A"
+							: " " + result.toFixed(2) + " " + sign}
+					</strong>
+				</p>
+			</fieldset>
+		</form>
+	);
+};
 
 export default Form;
