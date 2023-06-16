c0d55a39b1fac0b1b634941ed1e09aecef74874c
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-06-16 13:35:50 +0200

    updated showing on clock

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -2,33 +2,30 @@ import { useState, useEffect } from "react";
 import "./style.css";
 
 const Clock = () => {
-	const updateDate = () => {
-		let actualDate = new Date();
-		return actualDate.toLocaleString(undefined, {
-			weekday: "long",
-			day: "numeric",
-			month: "long",
-			hour: "2-digit",
-			minute: "2-digit",
-			second: "2-digit",
-		});
-	};
+  const [date, setDate] = useState(new Date());
 
-	const [dateResult, setDateResult] = useState(updateDate());
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(intervalID);
+  }, [date]);
 
-	useEffect(() => {
-		const intervalID = setInterval(() => {
-			setDateResult(updateDate());
-		}, 1000);
-		return () => clearInterval(intervalID);
-	}, [dateResult]);
+  const formatedDate = date.toLocaleString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 
-	return (
-		<p className="clock">
-			Dzisiaj jest&nbsp;
-			{dateResult}
-		</p>
-	);
+  return (
+    <p className="clock">
+      Dzisiaj jest&nbsp;
+      {formatedDate}
+    </p>
+  );
 };
 
 export default Clock;
