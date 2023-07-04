2ef5292d0fcaf5597e57b2fdc6ff362bfc4aa469
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-07-04 14:37:42 +0200

    take formatedDate function from useCurrentDate to index.js in Clock

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -2,12 +2,23 @@ import { StyledClock } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = () => {
+
+  const formatedDate = date =>
+    date.toLocaleString(undefined, {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+
   const date = useCurrentDate();
 
   return (
     <StyledClock>
       Dzisiaj jest&nbsp;
-      {date}
+      {formatedDate(date)}
     </StyledClock>
   );
 };
--- src/Form/Clock/useCurrentDate.js
+++ src/Form/Clock/useCurrentDate.js
@@ -10,14 +10,6 @@ export const useCurrentDate = () => {
     return () => clearInterval(intervalID);
   }, [date]);
 
-  const formatedDate = date =>
-    date.toLocaleString(undefined, {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    });
-  return formatedDate(date);
+  
+  return date;
 };
