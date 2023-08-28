c253147254965642b4943f0e03b9621af7623c92
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-08-28 17:01:53 +0200

    apiURL saved to  variable

--- src/useApi.js
+++ src/useApi.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 
 export const useApi = () => {
   const [currencies, setCurrencies] = useState({ requestState: "loading" });
-
+  const apiURL = "https://api.exchangerate.host/latest?base=PLN"
   useEffect(() => {
     const fetchCurrencies = async () => {
       try {
         await axios
-          .get("https://api.exchangerate.host/latest?base=PLN")
+          .get(apiURL)
           .then(response => setCurrencies(response.data));
           
       } catch (error) {
